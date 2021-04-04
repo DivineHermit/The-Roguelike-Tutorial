429514fafe45a01173d8bb7d79f609e5fa491cd0
Dominic Lee <34548479+divinehermit@users.noreply.github.com>
2021-04-04 20:09:20 +0100

    Basic dungeon creation started with two rooms and a corridor.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 
 def main() -> None:
@@ -33,7 +33,7 @@ def main() -> None:
     entities = {npc, player}
 
     # create an instance of the GameMap
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
 
     # create game engine for event handling and rendering
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
